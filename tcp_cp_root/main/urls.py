354f90dc588e57678401dee2f2c54ea9d43b07fe
LordVader23 <artem.fedotenko23@gmail.com>
2022-03-24 22:47:17 +0200

    Added url for detail page

--- tcp_cp_root/main/urls.py
+++ tcp_cp_root/main/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import index
+from .views import index, detail
 
 app_name = 'main'
 urlpatterns = [
+    path('films/<int:pk>/', detail, name='detail'),
     path('', index, name='index'),
 ]
