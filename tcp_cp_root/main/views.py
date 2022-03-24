8e84ac11dff93a975258f9da7779e77fb31a56b2
LordVader23 <artem.fedotenko23@gmail.com>
2022-03-24 22:47:37 +0200

    Wrote view for index page

--- tcp_cp_root/main/views.py
+++ tcp_cp_root/main/views.py
@@ -1,5 +1,25 @@
 from django.shortcuts import render
 
+from django.core.paginator import Paginator
+
+from .models import MovieSession
+
 
 def index(request):
-    return render(request, 'main/index.html')
+    mss = MovieSession.objects.all()  # change to raw sql later!!!
+
+    if 'page' in request.GET:
+        page_num = request.GET['page']
+    else:
+        page_num = 1
+
+    paginator = Paginator(mss, 9)
+    page = paginator.get_page(page_num)
+
+    context = {'mss': mss, 'page': page}
+
+    return render(request, 'main/index.html', context)
+
+
+def detail(request, pk):
+    pass
