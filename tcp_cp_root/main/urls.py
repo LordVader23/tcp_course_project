f2a3a790180fe3b3bb23d23e827ad944e56f6058
LordVader23 <artem.fedotenko23@gmail.com>
2022-03-26 19:40:31 +0200

    Added some urls

--- tcp_cp_root/main/urls.py
+++ tcp_cp_root/main/urls.py
@@ -1,8 +1,14 @@
 from django.urls import path
-from .views import index, detail
+from .views import index, detail, RegisterUserView, RegisterDoneView, LogoutView, ChangeUserInfoView
 
 app_name = 'main'
 urlpatterns = [
+    path('accounts/register/done/', RegisterDoneView.as_view(), name='register_done'),
+    path('accounts/register/', RegisterUserView.as_view(), name='register'),
+    path('accounts/logout/', LogoutView.as_view(), name='logout'),
+    path('accounts/profile/change/', ChangeUserInfoView.as_view(), name='profile_change'),
+    path('accounts/profile/', profile, name='profile'),
+    path('accounts/login/', login, name='login'),
     path('films/<int:pk>/', detail, name='detail'),
     path('', index, name='index'),
 ]
