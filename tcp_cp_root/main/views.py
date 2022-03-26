f646075d6b223cf73881a5de105be8ec00479387
LordVader23 <artem.fedotenko23@gmail.com>
2022-03-26 19:41:08 +0200

    Added views for register, login, logout, changeUserInfo

--- tcp_cp_root/main/views.py
+++ tcp_cp_root/main/views.py
@@ -1,12 +1,21 @@
+from django.contrib.auth import authenticate
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.views import LogoutView
+from django.contrib.messages.views import SuccessMessageMixin
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
 from django.shortcuts import get_object_or_404
 
 from django.core.paginator import Paginator
 
 from django.db.models import Q
+from django.urls import reverse_lazy
+from django.views.generic import CreateView, TemplateView, UpdateView
+from django.contrib.auth.models import User
+from django.contrib.auth import login as auth_login
 
 from .models import MovieSession
-from .forms import FilterForm
+from .forms import FilterForm, RegisterUserForm, ChangeInfoForm, LoginUserForm
 
 
 def index(request):
@@ -61,3 +70,83 @@ def detail(request, pk):
     context = {'ms': ms, 'form': form}
 
     return render(request, 'main/detail.html', context)
+
+# Auth views --------------------------------------------------------------
+
+
+class RegisterUserView(CreateView):
+    model = User
+    template_name = 'main/register_user.html'
+    form_class = RegisterUserForm
+    success_url = reverse_lazy('main:register_done')
+
+
+class RegisterDoneView(TemplateView):
+    template_name = 'main/register_done.html'
+
+
+def login(request, template_name='registration/login.html',
+          authentication_form=LoginUserForm):
+    if request.method == 'POST':
+        form = LoginUserForm(request.POST)
+
+        if form.is_valid():
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password']
+            remember_me = form.cleaned_data['remember_me']
+            user = authenticate(username=username, password=password)
+
+            if user:
+                auth_login(request, user)
+
+                if not remember_me:
+                    context = {'form': form}
+                    response = render(request, 'main/login.html', context)
+
+                    # keys = [key for key in request.session.keys()]
+                    # request.session[keys[0]].set_expiry(0)
+                    # ['_auth_user_id', '_auth_user_backend', '_auth_user_hash']
+                    # request.session['_auth_user_id'].set_expiry(0)
+                    request.session.set_expiry(0)
+
+                    # settings.SESSION_EXPIRE_AT_BROWSER_CLOSE = True
+
+                    return HttpResponseRedirect(reverse_lazy('main:index'))
+                else:
+                    return HttpResponseRedirect(reverse_lazy('main:profile'))
+            else:
+                form = LoginUserForm()
+                context = {'form': form}
+
+                return render(request, 'main/login.html', context)
+        else:
+            form = LoginUserForm()
+            context = {'form': form}
+
+            return render(request, 'main/login.html', context)
+    else:
+        form = LoginUserForm()
+        context = {'form': form}
+
+        return render(request, 'main/login.html', context)
+
+
+class LogoutView(LoginRequiredMixin, LogoutView):
+    template_name = 'main/logout.html'
+
+
+class ChangeUserInfoView(SuccessMessageMixin, LoginRequiredMixin, UpdateView):
+    model = User
+    template_name = 'main/change_user_info.html'
+    form_class = ChangeInfoForm
+    success_url = reverse_lazy('main:profile')
+    success_message = 'Личные данные пользователя изменены'
+
+    def dispatch(self, request, *args, **kwargs):
+        self.user_id = request.user.pk
+        return super().dispatch(request, *args, **kwargs)
+
+    def get_object(self, queryset=None):
+        if not queryset:
+            queryset = self.get_queryset()
+        return get_object_or_404(queryset, pk=self.user_id)
