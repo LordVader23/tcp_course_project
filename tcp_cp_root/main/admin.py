27f4ab87b9ef6c71fab4c2285b81f619fc4a5a96
LordVader23 <artem.fedotenko23@gmail.com>
2022-10-18 23:25:03 +0300

    Some fixes

--- tcp_cp_root/main/admin.py
+++ tcp_cp_root/main/admin.py
@@ -1,26 +1,26 @@
 from django.contrib import admin
 from django.utils.safestring import mark_safe
 
-from .models import Genre, Status, Seats, Movie, Payment, Booking, MovieSession
+from .models import Genres, Status, Seats, Movie, Payment, Booking, Moviesession
 
 
 class MovieAdm(admin.ModelAdmin):
-    list_display = ('movie_title', 'movie_country', 'movie_year', 'movie_duration', 'get_genres', 'get_img')
-    list_display_links = ('movie_title',)
-    fields = ('movie_title', 'movie_country', 'movie_year', 'movie_duration',
-              'movie_description', 'movie_genres', 'movie_image', 'get_img')
+    list_display = ('title', 'country', 'year', 'duration', 'get_genres', 'get_img')
+    list_display_links = ('title',)
+    fields = ('title', 'country', 'year', 'duration',
+              'description', 'movie_genres', 'image', 'get_img')
     readonly_fields = ('get_img',)
-    search_fields = ('id', 'movie_title', 'movie_country')
-    list_filter = ('movie_year', 'movie_genres')
+    search_fields = ('movie_id', 'title', 'country')
+    list_filter = ('year', 'movie_genres')
     list_per_page = 10
     list_max_show_all = 100
 
     def get_genres(self, obj):
-        return "\n".join([g.genre_name for g in obj.movie_genres.all()])
+        return "\n".join([g.name for g in obj.movie_genres.all()])
 
     def get_img(self, obj):
-        if obj.movie_image:
-            return mark_safe(f'<img src="{obj.movie_image.url}" width="120px"')
+        if obj.image:
+            return mark_safe(f'<img src="{obj.image.url}" width="120px"')
         else:
             return 'нет картинки'
 
@@ -29,29 +29,27 @@ class MovieAdm(admin.ModelAdmin):
 
 
 class BookingAdm(admin.ModelAdmin):
-    list_display = ('booking_owner', 'booking_session', 'booking_status',  'booking_code', 'booking_payment',
-                    'get_seats', 'booking_description', 'booking_date')
-    list_display_links = ('booking_owner', )
-    fields = ('booking_owner', 'booking_session', 'booking_status',
-              'booking_seats', 'booking_description', 'booking_date')
+    list_display = ('user', 'session', 'status',  'code', 'payment', 'get_seats', 'description', 'date')
+    list_display_links = ('user', )
+    fields = ('user', 'session', 'status', 'seats', 'description', 'date')
     readonly_fields = ('get_seats',)
-    search_fields = ('booking_date', )
-    list_filter = ('booking_status', )
-    list_editable = ('booking_status',)
+    search_fields = ('date', )
+    list_filter = ('status', )
+    list_editable = ('status',)
     list_per_page = 10
     list_max_show_all = 100
 
     def get_seats(self, obj):
-        return ", ".join([str(s.seats_number) for s in obj.booking_seats.all()])
+        return ", ".join([str(s.seat_number) for s in obj.seats.all()])
 
     get_seats.short_description = 'Места'
 
 
 class MovieSessionAdm(admin.ModelAdmin):
-    list_display = ('session_movie', 'session_date', 'session_price', 'get_booked_seats')
-    list_display_links = ('session_movie',)
-    fields = ('session_movie', 'session_date', 'session_price')
-    list_filter = ('session_movie', 'session_date')
+    list_display = ('movie', 'date', 'price', 'get_booked_seats')
+    list_display_links = ('movie',)
+    fields = ('movie', 'date', 'price')
+    list_filter = ('movie', 'date')
     list_per_page = 10
     list_max_show_all = 100
 
@@ -63,7 +61,7 @@ class MovieSessionAdm(admin.ModelAdmin):
 
 admin.site.register(Movie, MovieAdm)
 admin.site.register(Booking, BookingAdm)
-admin.site.register(MovieSession, MovieSessionAdm)
-admin.site.register(Genre)
+admin.site.register(Moviesession, MovieSessionAdm)
+admin.site.register(Genres)
 admin.site.register(Status)
 admin.site.register(Payment)
